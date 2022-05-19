f354463b7a1b941bdc6a9dec6cf692310525ecc3
AJHgit <andrew.heathcote@utas.edu.au>
2022-05-19 17:35:00 +0200

    Rename direcotry dmc -> emc

---
+++ emc/emc.R
@@ -0,0 +1,10 @@
+require(coda)
+source("emc/objects.R")    # pmwg object manipulation + mcmc creation functions
+source("emc/statistics.R") # chain statistics
+source("emc/plotting.R")   # chain and data plotting
+source("emc/data.R")       # data generation 
+source("emc/likelihood.R") # likelihoods
+source("emc/fitting.R")    # fitting automation
+source("emc/design.R")     # make design matrix and view parameters & mapping
+source("emc/priors.R")     # sampling from priors
+source("emc/pmwg.R")       # interface with pmwg
