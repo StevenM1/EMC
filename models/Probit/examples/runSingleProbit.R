216c12de08df085f9bd3d1b99ff460a72741757f
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2022-05-27 10:00:11 +0200

    Restructured models directory to add TYPE layer

--- models/Probit/examples/runSingleProbit.R
+++
@@ -1,7 +0,0 @@
-rm(list=ls())
-source("emc/emc.R")
-source("models/Probit/SDTgaussian.R")
-
-load("probitIndividual.RData")
-samples <- run_chains(samplers,iter=c(1000,0,0))
-save(samples,file="probitIndividual.RData")
