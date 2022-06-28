42256dfc9f5f7c9bd63286213c78cd5ed28f4345
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2022-06-28 11:43:35 +0200

    Moved bad parameter range check for DDM into model file

--- emc/likelihood.R
+++ emc/likelihood.R
@@ -182,10 +182,10 @@ log_likelihood_ddm <- function(p_vector,dadm,min_ll=log(1e-10))
 {
   pars <- get_pars(p_vector,dadm)
   like <- numeric(dim(dadm)[1]) 
-  bad <- abs(pars[,"v"])>5 | pars[,"a"]>2 | pars[,"sv"]>2 | pars[,"sv"]<.1 | pars[,"SZ"]>.75 | pars[,"SZ"]<.01 | pars[,"st0"]>.2
-  if (any(!bad)) 
-    like[!bad] <- attr(dadm,"model")$dfun(dadm$rt[!bad],dadm$R[!bad],pars[!bad,,drop=FALSE])
-  like[!bad][is.na(like[!bad])] <- 0
+  # bad <- abs(pars[,"v"])>5 | pars[,"a"]>2 | pars[,"sv"]>2 | pars[,"sv"]<.1 | pars[,"SZ"]>.75 | pars[,"SZ"]<.01 | pars[,"st0"]>.2
+  if (any(attr(pars,"ok"))) 
+    like[attr(pars,"ok")] <- attr(dadm,"model")$dfun(dadm$rt[attr(pars,"ok")],dadm$R[attr(pars,"ok")],pars[attr(pars,"ok"),,drop=FALSE])
+  like[attr(pars,"ok")][is.na(like[attr(pars,"ok")])] <- 0
   sum(pmax(min_ll,log(like[attr(dadm,"expand")])))
 }
 
@@ -216,7 +216,12 @@ log_likelihood_sdt <- function(p_vector,dadm,lb=-Inf,min_ll=log(1e-10))
   # Shorten expand to only one per lR set
   expand <- (attr(dadm,"expand")[(c(1:ne) %% nr)==0] + 1 ) %/% nr
   # log probability
-  ll <- log(attr(dadm,"model")$pfun(lt=lt,ut=ut,pars=pars[dadm$winner,]))[expand]
+  ll <- numeric(sum(dadm$winner))
+  # Bad parameter region
+  ok <- attr(pars,"ok")
+  okw <- ok[dadm$winner]
+  ll[ok] <- log(attr(dadm,"model")$pfun(lt=lt[okw],ut=ut[okw],pars=pars[dadm$winner & ok,]))
+  ll <- ll[expand]
   ll[is.na(ll)] <- 0
   sum(pmax(min_ll,ll))
 }
