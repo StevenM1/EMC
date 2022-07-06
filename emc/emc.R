d88fd3a782c180ab88ae0d335b61bd177723b2f2
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2022-07-06 13:18:04 +0200

    Updated packages

--- emc/emc.R
+++ emc/emc.R
@@ -1,14 +1,13 @@
-require(magic)
-require(coda)
-require(MCMCpack)
-require(invgamma)
-require(corpcor)
-require(condMVNorm)
-require(parallel)
-require(MASS) 
-require(abind)
-require(rtdists)
-require(mvtnorm)
+require(coda)        # EMC
+require(rtdists)     # EMC
+require(magic)       # PMwG
+require(MASS)        # PMwG
+require(abind)       # PMwG
+require(MCMCpack)    # PMwG IS2
+require(corpcor)     # PMwG IS2
+require(condMVNorm)  # PMwG IS2
+require(parallel)    # PMwG IS2
+require(mvtnorm)     # PMwG IS2
 
 
 source("emc/objects.R")    # pmwg object manipulation + mcmc creation functions
