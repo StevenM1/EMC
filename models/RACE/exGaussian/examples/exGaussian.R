70bf474f9a1266b1d960be754e26d378e6b219e7
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2022-07-06 13:18:47 +0200

    New exGaussian lesson

--- models/RACE/exGaussian/examples/exGaussian.R
+++ models/RACE/exGaussian/examples/exGaussian.R
@@ -1,47 +1,113 @@
 rm(list=ls())
-library(mvtnorm)
 source("emc/emc.R")
 source("models/RACE/exGaussian/exGaussian.R")
 
+# PNAS data
+print( load("Data/PNAS.RData"))
+dat <- data[,c("s","E","S","R","RT")]
+names(dat)[c(1,5)] <- c("subjects","rt")
+levels(dat$R) <- levels(dat$S)
 
-# Two choice normal
-designEXG <- make_design(Flist=list(mu ~ lM, sigma ~ 1, tau ~ 1),
-  Ffactors=list(subjects=1,S=1:2),Rlevels=1:2,matchfun=function(d)d$S==d$lR,
-  Clist=list(lM=matrix(c(-1/2,1/2),ncol=1),S=contr.helmert,lR=contr.helmert),
+# Often descriptive analyses with the ExGaussian are done only on correct 
+# responses. 
+dat <- dat[dat$S==dat$R,]
+
+# Fit to individual design cells, this will be explained in later lessons, for
+# now just note this enables us to fit an 18 parameter model, with separate mu,
+# sigma and tau estimates for each design cell. 
+se <- function(d) {factor(paste(d$S,d$E,sep="_"),levels=
+  c("left_accuracy","right_accuracy","left_neutral","right_neutral","left_speed","right_speed"))}
+
+designEXG <- make_design(
+  Ffactors=list(subjects=levels(dat$subjects),S=levels(dat$S),E=levels(dat$E)),
+  Rlevels=levels(dat$R),
+  Clist=list(SE=diag(6)),
+  Flist=list(mu~0+SE,sigma~0+SE,tau~0+SE),
+  Ffunctions = list(SE=se),
   model=exGaussian)
 
-p_vector <- sampled_p_vector(designEXG)
-p_vector[1:2] <- log(c(.5,.5))
-p_vector[3:4] <- log(c(.2,.4)) # sd
+# samplers <- make_samplers(dat,designEXG,type="standard",rt_resolution=.02)
+# save(samplers,file="sPNAS_exg.RData")
 
-dataEXG <- make_data(p_vector,design=designEXG,trials=1000)
-plot_defective_density(dataEXG,layout=c(1,2))
-
-dadmEXG <- design_model(dataEXG,designEXG)
-par(mfrow=c(1,4))
-profile_pmwg(pname="mu",p=p_vector,p_min=log(.25),p_max=log(.75),dadm=dadmEXG)
-profile_pmwg(pname="mu_lM1",p=p_vector,p_min=log(.25),p_max=log(.75),dadm=dadmEXG)
-profile_pmwg(pname="sigma",p=p_vector,p_min=log(.1),p_max=log(.3),dadm=dadmEXG)
-profile_pmwg(pname="tau",p=p_vector,p_min=log(.3),p_max=log(.5),dadm=dadmEXG)
+print(load("models/RACE/exGaussian/examples/samples/sPNAS_exg.RData"))
+check_run(samplers,layout=c(3,6),width=12,subfilter=500)
 
 
-# This example shows EMC works in a model without choice (Rlevels=1, no matchfun)
-# Make a reasonably complex design. 
-designEXG <- make_design(Flist=list(mu ~ A*B, sigma ~ B, tau ~ 1),
-  Ffactors=list(subjects=50,A=1:3,B=1:2),Rlevels=1,model=exGaussian)
+#### Check model fit ----
 
-p_vector <- sampled_p_vector(designEXG)
-p_vector[1:6] <- log(c(.5,1.2,1.4,1.5,1,1)) # Increasing in A and B, no interaction
-p_vector[7:9] <- log(c(.1,2,.3)) # more variable for B2 than B1 
+# post predict did not use first 500, so inference based on 2000*3 samples
+plot_fit(dat,pp,layout=c(2,3),factors=c("E","S"),lpos="right",xlim=c(.25,1.5))
+plot_fits(dat,pp,layout=c(2,3),lpos="right")
+
+# Very accurate fits
+tab <- plot_fit(dat,pp,layout=c(2,3),factors=c("E","S"),
+         stat=function(d){mean(d$rt)},stat_name="Mean RT (s)",xlim=c(0.375,.625))
+tab <- plot_fit(dat,pp,layout=c(2,3),factors=c("E","S"),xlim=c(0.275,.4),
+         stat=function(d){quantile(d$rt,.1)},stat_name="10th Percentile (s)")
+tab <- plot_fit(dat,pp,layout=c(2,3),factors=c("E","S"),xlim=c(0.525,.975),
+         stat=function(d){quantile(d$rt,.9)},stat_name="90th Percentile (s)")
+tab <- plot_fit(dat,pp,layout=c(2,3),factors=c("E","S"),
+         stat=function(d){sd(d$rt)},stat_name="SD (s)",xlim=c(0.1,.355))
 
 
-dataEXG <- make_data(p_vector,design=designEXG,trials=1000)
-plot_defective_density(dataEXG,layout=c(2,3),rt="topleft")
+### Examine population mean (mu) parameters
 
-dadmEXG <- design_model(dataEXG,designEXG)
-par(mfrow=c(2,5))
-for (i in names(p_vector))
-  profile_pmwg(pname=i,p=p_vector,p_min=p_vector[i]-.5,p_max=p_vector[i]+.5,dadm=dadmEXG)
+# Priors all well dominated 
+ci <- plot_density(samplers,layout=c(3,6),selection="mu",subfilter=500)
+# Now on the natural scale
+ci_mapped <- plot_density(samplers,layout=c(3,6),subfilter=500,
+                                        selection="mu",mapped=TRUE)
 
-samplers <- make_samplers(dataEXG,designEXG,type="standard",rt_resolution=.001)
+# Looking at parameters on the natural scale
+round(ci,2)
+round(ci_mapped,2)
+
+# skew: accuracy > neutral > speed
+p_test(x=samplers,mapped=TRUE,x_name="left skew: accuracy - speed",
+  x_fun=function(x){x["tau_left_accuracy"]-x["tau_left_speed"]})
+
+p_test(x=samplers,mapped=TRUE,x_name="right skew: accuracy - speed",
+  x_fun=function(x){x["tau_right_accuracy"]-x["tau_right_speed"]})
+
+p_test(x=samplers,mapped=TRUE,x_name="left skew: accuracy - neutral",
+  x_fun=function(x){x["tau_left_accuracy"]-x["tau_left_neutral"]})
+
+p_test(x=samplers,mapped=TRUE,x_name="right skew: accuracy - neutral",
+  x_fun=function(x){x["tau_right_accuracy"]-x["tau_right_neutral"]})
+
+# Mean RT: accuracy > neutral > speed
+p_test(x=samplers,mapped=TRUE,x_name="left mean: accuracy - speed",
+  x_fun=function(x){sum(x[c("mu_left_accuracy","tau_left_accuracy")]) - 
+                    sum(x[c("mu_left_speed","tau_left_speed")])})
+
+p_test(x=samplers,mapped=TRUE,x_name="right mean: accuracy - speed",
+  x_fun=function(x){sum(x[c("mu_right_accuracy","tau_right_accuracy")]) - 
+                    mean(x[c("mu_right_speed","tau_right_speed")])})
+
+p_test(x=samplers,mapped=TRUE,x_name="left mean: accuracy - neutral",
+  x_fun=function(x){sum(x[c("mu_left_accuracy","tau_left_accuracy")]) - 
+                    sum(x[c("mu_left_neutral","tau_left_neutral")])})
+
+p_test(x=samplers,mapped=TRUE,x_name="right mean: accuracy - neutral",
+  x_fun=function(x){sum(x[c("mu_right_accuracy","tau_right_accuracy")]) - 
+                    sum(x[c("mu_right_neutral","tau_right_neutral")])})
+
+# SD R: accuracy > neutral > speed
+p_test(x=samplers,mapped=TRUE,x_name="left SD: accuracy - speed",
+  x_fun=function(x){sqrt(sum(x[c("mu_left_accuracy","tau_left_accuracy")]^2)) - 
+                    sqrt(sum(x[c("mu_left_speed","tau_left_speed")]^2))})
+
+p_test(x=samplers,mapped=TRUE,x_name="right SD: accuracy - speed",
+  x_fun=function(x){sqrt(sum(x[c("mu_right_accuracy","tau_right_accuracy")]^2)) - 
+                    sqrt(sum(x[c("mu_right_speed","tau_right_speed")]^2))})
+
+p_test(x=samplers,mapped=TRUE,x_name="left SD: accuracy - neutral",
+  x_fun=function(x){sqrt(sum(x[c("mu_left_accuracy","tau_left_accuracy")]^2)) - 
+                    sqrt(sum(x[c("mu_left_neutral","tau_left_neutral")]^2))})
+
+p_test(x=samplers,mapped=TRUE,x_name="right SD: accuracy - neutral",
+  x_fun=function(x){sqrt(sum(x[c("mu_right_accuracy","tau_right_accuracy")]^2)) - 
+                    sqrt(sum(x[c("mu_right_neutral","tau_right_neutral")]^2))})
+
+
 
